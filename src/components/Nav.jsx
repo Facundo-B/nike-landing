941bb44c10e09e479d53860f103a917affd2846b
Facundo-B <fmblanco97@gmail.com>
2024-03-27 15:39:23 -0300

    Hero text section done.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ function Nav() {
                     {
                         navLinks.map((item) => (
                             <li key={item.label}>
-                                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>{item.label}</a>
+                                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray font-semibold'>{item.label}</a>
                             </li>
                         ))
                     }
