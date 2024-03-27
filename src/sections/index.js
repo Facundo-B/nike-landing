2792996e738d6e20e170eae9394f71c66867da22
Facundo-B <fmblanco97@gmail.com>
2024-03-27 13:29:24 -0300

    Added component files. Developing Nav component.

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero"
+import PopularProducts from "./PopularProducts"
+import SuperQuality from "./SuperQuality"
+import Subscribe from "./Subscribe"
+import SpecialOffer from "./SpecialOffer"
+import CustomerReviews from "./CustomerReviews"
+import Footer from "./Footer"
+import Services from "./Services"
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer,
+    CustomerReviews,
+    Footer,
+    Services
+}
