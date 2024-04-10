37c5c2099a581f145c28b964a1c8686dedafb43f
Facundo-B <fmblanco97@gmail.com>
2024-04-10 16:58:24 -0300

    add show image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
 import { statistics } from '../constants'
+import { bigShoe1 } from '../assets/images'
 
 function Hero() {
   return (
@@ -26,7 +27,9 @@ function Hero() {
           ))}
         </div>
       </div>
-
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-hero bg-cover bg-center bg-primary'>
+        <img src={bigShoe1} alt="shoe collection" width={600} height={500} className="object-contain relative z-10"/>
+      </div>
 
     </section>
   )
