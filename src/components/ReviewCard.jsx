132626ed1809aebc7592fc5434e43ede551e3eab
Facundo-B <fmblanco97@gmail.com>
2024-04-14 19:33:14 -0300

    CustomerReviews section done

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { star } from '../assets/icons'
+
+const ReviewCard = ({ imgURL, rating, customerName, feedback }) => {
+    return (
+        <div className='flex justify-center items-center flex-col'>
+            <img src={imgURL} alt="customer" className='rounded-full object-cover w-[120px] h-[120px]' />
+            <p className='mt-6 max-w-sm text-center info-text'>{feedback}</p>
+            <div className='mt-3 flex justify-center items-center gap-2'>
+                <img src={star} className='w-[24px] h-[24px] object-contain m-0' />
+                <p className='text-xl font-montserrat text-slate-gray font-semibold'>({rating})</p>
+            </div>
+            <h3 className='mt-1 font-palanquin text-3xl text-center font-semibold'>{customerName}</h3>
+        </div>
+    )
+}
+
+export default ReviewCard
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { reviews } from '../constants'
+import ReviewCard from '../components/ReviewCard'
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className='max-container'>
+      <h3 className='text-4xl font-bold font-palanquin text-center'>What Our <span className='text-coral-red'>Custumers</span> Say</h3>
+      <p className='m-auto mt-4 text-center info-text max-w-lg'>Hear genuine stories from out satisfied customers</p>
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {
+          reviews.map((review)=>(
+            <ReviewCard key={review.customerName} {...review}/>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
