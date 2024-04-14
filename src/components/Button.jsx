a01df15abea4457ce3d96c6f1514d4f1d1b83119
Facundo-B <fmblanco97@gmail.com>
2024-04-14 14:55:32 -0300

    SuperQuality and Services sections done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-function Button({label, iconURL}) {
+function Button({ label, iconURL }) {
   return (
     <button className='flex justify-center items-center gap-1 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
-        {label}
-        <img className='ml-2 rounded-full w-5 h-5' src={iconURL} alt="arrow-right" />
+      {label}
+      {iconURL && <img className='ml-2 rounded-full w-5 h-5' src={iconURL} alt="arrow-right" />
+      }
+
     </button>
   )
 }
